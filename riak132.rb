3938c61e30c2e0936766b6b2c1fef04cd7b8acf5
Pasha Lifshiz <plifshiz@rentpath.com>
2014-11-19 21:58:33 -0500

    [PL] [00000] updating version according to brew standard

--- riak132.rb
+++ riak132.rb
@@ -3,8 +3,8 @@ require "formula"
 class Riak132 < Formula
   homepage "http://basho.com/riak/"
   url "http://s3.amazonaws.com/downloads.basho.com/riak/1.3/1.3.2/osx/10.8/riak-1.3.2-osx-x86_64.tar.gz"
-  version "1.3.2"
   sha256 "3a31e7dd00487b4758307d9932a508401ed1763ed3360cbe8ca9615e2ffd7c0e"
+  version 1
 
   skip_clean 'libexec'
 
